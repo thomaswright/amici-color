d589ab2942f325f43b54df51727f12930a1dc5d5
Thomas Wright <thomaswright1@gmail.com>
2024-09-24 11:16:01 +0900

    add homepage

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://thomaswright.github.io/amici-color",
   plugins: [
     svgr(),
     react({
